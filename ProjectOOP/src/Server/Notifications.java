065e736534a27f2941f0dd7b34085f38a7bc0b42
Kevin <Kevin@10.55.29.76>
2018-07-09 13:24:35 +0300

    Documentation for Notification Class added.

--- ProjectOOP/src/Server/Notifications.java
+++ ProjectOOP/src/Server/Notifications.java
@@ -10,7 +10,24 @@ import java.awt.event.*;
 import javax.swing.*;
 
 /**
- *
+ * This is a Notifications Class used to setup and display notifications to the
+ * User on the system tray. It is a subclass of the TrayIcon class. A TrayIcon 
+ * object represents a tray icon that can be added to the system tray. A TrayIcon 
+ * can have a tooltip (text), an image, a pop up menu, and a set of listeners 
+ * associated with it. A TrayIcon can generate various MouseEvents and supports
+ * adding corresponding listeners to receive notification of these events. TrayIcon 
+ * processes some of the events by itself. For example, by default, when the right-mouse
+ * click is performed on the TrayIcon it displays the specified pop up menu. When the 
+ * mouse hovers over the TrayIcon the tooltip is displayed.<br>Note: When the MouseEvent
+ * is dispatched to its registered listeners its component property will be set to null. 
+ * (See ComponentEvent.getComponent()) The source property will be set to this TrayIcon.
+ * (See EventObject.getSource()). This class adds a mouseClicked event and a pop up menu
+ *  automatically by calling its relevant methods. It also adds this TrayIcon object to the 
+ * system tray automatically with a call to displayTray method If a SecurityManager is installed,  
+ * the AWTPermission accessSystemTray must be granted in order to create a TrayIcon. 
+ * Otherwise the constructor will throw a SecurityException.<br>
+ * See the SystemTray class overview for an example on how to use the TrayIcon API.
+ * @version 1.0
  * @author Kevin
  */
 public class Notifications extends TrayIcon {
