86b97cdf8f9db371bfc64e051cbab0b69c62d1f2
Kevin <Kevin@10.55.29.76>
2018-07-09 13:25:53 +0300

    Documentation for Notification Class added.

--- ProjectOOP/src/Server/Notifications.java
+++ ProjectOOP/src/Server/Notifications.java
@@ -1,7 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * There exists no licence for this product. One may do with it 
+ * as they please.
  */
 package Server;
 
